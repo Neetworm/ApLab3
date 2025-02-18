b7c5d4ada5390950aa757cd1073b7e754a23924d
Neetworm <221b160@juetguna.in>
2025-02-18 06:19:32 +0000

    AbsTest and Testable updated.

--- program_2/TestableExample.java
+++ program_2/TestableExample.java
@@ -2,9 +2,9 @@
 public class TestableExample {
     public static void main(String[] args) {
         Test test = new Test();
-        test.display();  // This will print: "This is a concrete class implementing Testable interface."
+        test.display();  
 
         ConcreteTest concreteTest = new ConcreteTest();
-        concreteTest.display();  // This will print: "This is a concrete subclass of AbsTest implementing display method."
+        concreteTest.display();  
     }
 }
